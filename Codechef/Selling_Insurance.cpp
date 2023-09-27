69dff49dfd736d35d3831d7dfe8ede586b392f5f
Vishal-S-Hayyal <vh1835@srmist.edu.in>
2023-09-27 23:19:38 +0530

    Codechef 102

---
+++ Codechef/Selling_Insurance.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    float x;
+	    cin>>x;
+	    float y=0;
+	    float k=5/x;
+	    y=100*k;
+	    cout<<ceil(y)<<endl;
+	}
+	return 0;
+}
