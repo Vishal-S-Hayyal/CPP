b4d5ca3e5be10db99ca7f3b14d583a25f2e33104
Vishal-S-Hayyal <vh1835@srmist.edu.in>
2023-10-24 23:00:54 +0530

    24/10/2023

---
+++ TCS CODEVITA/prime_construction.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isprime(int n){
+    for(int i=2; i<=n/2; i++){
+        if((n%i)==0){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+}
+
+int cal(vector<int> numbers, int min){
+    int ans=1;
+    for(int i=0; i<numbers.size(); i++){
+         ans = ans*nuumbers[i]
+    }
+    int final= (ans/min)
+}
+
+int main() {
+    int x;
+    cin>>x;
+    x+=1;
+    
+}
